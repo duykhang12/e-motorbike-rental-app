92a5c63a7dac7ec3e765e65df0754759c7525304
KhaiTrinhVan <trinhvankhai61@gmail.com>
2025-09-06 08:15:04 +0700

    Register.h

---
+++ Register.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <User.h>    //have file User.h
+using namespace std;
+class User;
+class Register{
+    private:
+    vector <User> users;
+    int newID = 1;
+
+    public:
+    void registerUser(){};
+};
